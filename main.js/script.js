62be4b2a17874b31463085615bdf90cc56db8939
io.FortuNato <nomefortunato60@gmail.com>
2026-02-02 20:35:39 +0100

    add calculated price

--- main.js/script.js
+++ main.js/script.js
@@ -1,4 +1,9 @@
-let priceKm = 0.21;
-console.log (priceKm)
+let priceKm = 21;
+let percentualeMinori = 20;
+let percentualeAnziani = 40;
 
+let primoSconto = Math.round(priceKm * (1 - percentualeMinori / 100));
+console.log (primoSconto);
 
+let secondoSconto = Math.round(priceKm * (1 - percentualeAnziani / 100));
+console.log (secondoSconto)
