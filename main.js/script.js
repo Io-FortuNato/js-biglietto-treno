0e8796e53d3c7564dafe3b314ef7f8b4f8281470
io.FortuNato <nomefortunato60@gmail.com>
2026-02-02 20:49:31 +0100

    add variables of scount

--- main.js/script.js
+++ main.js/script.js
@@ -1,4 +1,4 @@
-let priceKm = 21;
+let priceKm = 21; // centesimi
 let percentualeMinori = 20;
 let percentualeAnziani = 40;
 
@@ -7,3 +7,13 @@ console.log (primoSconto);
 
 let secondoSconto = Math.round(priceKm * (1 - percentualeAnziani / 100));
 console.log (secondoSconto)
+
+let età = 66;
+
+if (età < 18) {
+   console.log ("hai dirtto al primoSconto");
+} else if (età > 65) {
+   console.log ("hai diritto al secondoSconto");
+} else {
+    console.log ("non hai diritto allo sconto")
+}
