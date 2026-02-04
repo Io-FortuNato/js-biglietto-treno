76765cb4d730ebd594cabcb418663a6e8d69005e
io.FortuNato <nomefortunato60@gmail.com>
2026-02-04 17:15:36 +0100

    fixed work

--- main.js/script.js
+++ main.js/script.js
@@ -3,21 +3,16 @@ let km = Number(prompt("Quanti km vuoi percorrere?:"));
 let prezzo = km * priceKm;
 let percentualeMinori = 20;
 let percentualeAnziani = 40;
-let età = Number(prompt("Inserisci la tua età:"));
+let eta = Number(prompt("Inserisci la tua età:"));
 
 
-if (età < 18) {
-   console.log ("hai dirtto al primoSconto");
-} else if (età > 65) {
-   console.log ("hai diritto al secondoSconto");
-} else {
-    console.log ("non hai diritto allo sconto")
-}
-
-if (età < 18) {
- prezzo = Math.round (prezzo * (1 - percentualeMinori / 100));
-} else if (età > 65) {
+if (eta < 18) {
+    console.log("hai diritto allo sconto under 18");
+    prezzo = Math.round (prezzo * (1 - percentualeMinori / 100));
+} else if (eta > 65) {
+    console.log("hai diritto allo sconto over 65");
     prezzo = Math.round (prezzo *(1 - percentualeAnziani / 100));
-} else {prezzo}
+} else {prezzo,console.log("nessuno sconto disponibile");
+}
 
 console.log("totale:",(prezzo / 100).toFixed(2) + " £")
